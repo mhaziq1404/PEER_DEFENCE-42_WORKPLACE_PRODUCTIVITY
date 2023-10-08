698568ed48fbe28ce111f51da6c6cb9504b4029e
Zixianlow <113102979+Zixianlow@users.noreply.github.com>
2023-10-08 14:43:58 +0800

    update main.py

--- main.py
+++ main.py
@@ -142,6 +142,24 @@ def generate_sheet():
     print("\033[0;34mExit the program and type 'open .' to view the folder and get the excel\033[0m")
     print(f"\033[0;32mK Bye.\033[0m")
 
+
+def make_graph(list: list):
+    dict = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
+    for item in list:
+        if (item['level'] >= 8):
+            item['level'] = 8
+        dict[int(item['level'])] += 1
+    # plt.bar(dict.keys(), dict.values())
+    # plt.xlabel('Cadets level')
+    # plt.ylabel('Head counts')
+    # plt.xticks([0, 1, 2, 3, 4, 5, 6, 7, 8])
+    # plt.title('Selected cadets level distribution')
+    # plt.savefig('bar_chart.png') 
+    # # Display the plot
+    # plt.close()
+    return (dict)
+
+
 # This is when the program is ran
 print("\033[33m=========DHARMA GENERATOR========\033[0m     \033[31mby jakoh, bshamsid\033[0m")
 print("\u001b[33mTo exit program type: 'exit'\033[0m")
